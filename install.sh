227551e8c100db7f7e357d707433e171cc83958c
kleidione Freitas <kleidione@gmail.com>
2021-03-15 17:44:49 +0000

    Corrigido linha referente a copia de arquivos do user a home

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp      -R {Wallpapers,.config,.scripts,.xinitrc,.bashrc,.Xresources} ~/
+cp -R {Wallpapers,.config,.scripts,.xinitrc,.bashrc,.Xresources} ~/
 sudo cp -R {vimix,vimix-dark} /usr/share/themes/
 sudo cp -R xorg.conf.d /etc/X11/
 sudo cp -R dwm /usr/src/ ; cd /usr/src/dwm/ && sudo make clean install
