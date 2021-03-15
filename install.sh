731808b5875f09fd652b6a1de816a3f18611a272
kleidione Freitas <kleidione@gmail.com>
2021-03-15 17:40:08 +0000

    Corrigido instalação do papirus

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo cp -R {vimix,vimix-dark} /usr/share/themes/
 sudo cp -R xorg.conf.d /etc/X11/
 sudo cp -R dwm /usr/src/ ; cd /usr/src/dwm/ && sudo make clean install
 
-sudo wget wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sh
-sudo wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/master/install.sh | sh
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install.sh | sudo sh
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-folders/master/install.sh | sudo sh
 sudo papirus-folders --color brown
 
 exit
